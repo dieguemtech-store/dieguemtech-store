52a621343c7b961372fee81e9ede4eddb4525a38
dieguemtech-store <mboup32241@gmail.com>
2026-06-18 20:09:58 +0000

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const ordersFile = path.join(__dirname, "data", "orders.json");
 app.disable("x-powered-by");
 
 app.use(express.json({ limit: "100kb" }));
-app.use("/Actifs", express.static(path.join(__dirname, "Actifs")));
+app.use("/Assets", express.static(path.join(__dirname, "Assets")));
 
 app.get("/api/health", (req, res) => {
   res.json({
